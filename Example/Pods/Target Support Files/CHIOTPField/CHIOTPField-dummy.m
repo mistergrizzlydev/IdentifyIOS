03df3cec081f8eefede2f31fcb6d855ff1b9f157
Emir <emir@beytekin.net>
2021-06-15 18:15:04 +0300

    - kurulu podlar kaldırıldı

--- Example/Pods/Target Support Files/CHIOTPField/CHIOTPField-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CHIOTPField : NSObject
-@end
-@implementation PodsDummy_CHIOTPField
-@end
--- Example/Pods/Target Support Files/IdentifyIOS/IdentifyIOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_IdentifyIOS : NSObject
-@end
-@implementation PodsDummy_IdentifyIOS
-@end
